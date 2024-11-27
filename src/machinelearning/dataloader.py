4fdfc94feec18ab4b595f54ce6ebea7da315af11
Ingrid Galje-Bol <ingrid.bol@student.hu.nl>
2024-11-27 18:54:45 +0000

    Kleine aanpassing dataloader

--- src/machinelearning/dataloader.py
+++ src/machinelearning/dataloader.py
@@ -6,7 +6,7 @@ class DataLoader:
     def __init__(self, image_folder: Path):
         self.image_folder = image_folder
 
-    def walk_dir(path: Path) -> Iterator:
+    def walk_dir(self, path: Path) -> Iterator:
         """loops recursively through a folder
 
         Args:
@@ -19,12 +19,9 @@ class DataLoader:
 
         for p in Path(path).iterdir():
             if p.is_dir():
-                yield from walk_dir(p)
+                yield from self.walk_dir(p)
                 continue
             # resolve works like .absolute(), but it removes the "../.." parts
             # of the location, so it is cleaner
             yield p.resolve()
 
-    # Gebruik de functie en print de resultaten
-    for file_path in walk_dir(image_folder):
-        print(file_path)
