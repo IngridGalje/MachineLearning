03d542e1df0b42bd82c5e413a2bf7ba4970c3f3e
Ingrid Galje-Bol <ingrid.bol@student.hu.nl>
2024-12-05 16:19:58 +0000

    notebook voor baseRNN gemaakt en getest

--- src/machinelearning/filehandling.py
+++ src/machinelearning/filehandling.py
@@ -1,27 +1,30 @@
 from pathlib import Path
-import numpy as np
-from typing import Iterator, Tuple, List
+from typing import Iterator
 
 class DataLoader:
-    def __init__(self, image_folder: Path):
-        self.image_folder = image_folder
+    def __init__(self, folder: Path):
+        self.folder = folder
 
-    def walk_dir(self, path: Path) -> Iterator:
+    def walk_dir(self, path: Path) -> Iterator[Path]:
         """loops recursively through a folder
 
         Args:
-            path (Path): folder to loop trough. If a directory
+            path (Path): folder to loop through. If a directory
                 is encountered, loop through that recursively.
 
         Yields:
             Generator: all paths in a folder and subdirs.
         """
-
         for p in Path(path).iterdir():
             if p.is_dir():
                 yield from self.walk_dir(p)
                 continue
-            # resolve works like .absolute(), but it removes the "../.." parts
-            # of the location, so it is cleaner
             yield p.resolve()
 
+    def print_file_paths(self, limit: int = 5):
+        count = 0
+        for file_path in self.walk_dir(self.folder):
+            if count >= limit:
+                break
+            print(file_path)
+            count += 1
--- src/machinelearning/main.py
+++ src/machinelearning/main.py
@@ -4,16 +4,31 @@ from typing import Iterator, Tuple, List
 import mads_datasets
 mads_datasets.__version__
 
-from filehandling import Dataloader
+# Gebruik absolute import
+from machinelearning.filehandling import DataLoader
+from mads_datasets.settings import FileTypes
 
-dataloader = DataLoader(Path("data/images"))
+from mads_datasets import DatasetFactoryProvider, DatasetType
 
-for file_path in walk_dir(image_folder):
-        print(file_path)
+flowersfactory = DatasetFactoryProvider.create_factory(DatasetType.FLOWERS)
+flowersfactory.download_data()
+
+image_folder = flowersfactory.subfolder
+print(image_folder)
+
+def dataloader(image_folder: Path):
+    dataloader = DataLoader(image_folder)
+    dataloader.walk_dir(image_folder)
+    dataloader.print_file_paths(limit=5)
+
+
+#print filetypes
+for ft in FileTypes:
+    print(ft)    
 
 # src/mymodule/main.py
 def main():
-    print("Hello World")
+    dataloader(image_folder)
 
 if __name__ == '__main__':
     main()
